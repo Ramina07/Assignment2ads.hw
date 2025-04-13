c138458504de93244528ad9bf8686e0d8bdd5a57
Acer <@Ramina_k>
2025-04-13 23:06:26 +0500

    Refactored MyArrayList and added documentation comments

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,4 +1,6 @@
-// MyArrayList.java (completed implementation)
+/**
+ * MyArrayList is a custom implementation of a dynamic array.
+ */
 public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
     private Object[] data;
     private int size;
@@ -9,6 +11,7 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
         size = 0;
     }
 
+    // Doubles array capacity when full
     private void ensureCapacity() {
         if (size >= data.length) {
             Object[] newData = new Object[data.length * 2];
@@ -17,31 +20,31 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
         }
     }
 
-    @Override
+    // Adds an item to the end
     public void add(T item) {
         ensureCapacity();
         data[size++] = item;
     }
 
-    @Override
+    // Returns element at index
     public T get(int index) {
         checkIndex(index);
         return (T) data[index];
     }
 
-    @Override
+    // Removes element by index
     public void remove(int index) {
         checkIndex(index);
         System.arraycopy(data, index + 1, data, index, size - index - 1);
         data[--size] = null;
     }
 
-    @Override
+    // Returns current number of elements
     public int size() {
         return size;
     }
 
-    @Override
+    // Sorts elements using bubble sort
     public void sort() {
         for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
@@ -55,7 +58,6 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
         }
     }
 
-    @Override
     public int indexOf(Object object) {
         for (int i = 0; i < size; i++) {
             if (data[i].equals(object)) return i;
@@ -63,7 +65,6 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
         return -1;
     }
 
-    @Override
     public int lastIndexOf(Object object) {
         for (int i = size - 1; i >= 0; i--) {
             if (data[i].equals(object)) return i;
@@ -71,34 +72,31 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
         return -1;
     }
 
-    @Override
     public boolean exists(Object object) {
         return indexOf(object) != -1;
     }
 
-    @Override
     public Object[] toArray() {
         Object[] result = new Object[size];
         System.arraycopy(data, 0, result, 0, size);
         return result;
     }
 
-    @Override
     public void clear() {
         for (int i = 0; i < size; i++) data[i] = null;
         size = 0;
     }
 
-    @Override public void set(int index, T item) {}
-    @Override public void add(int index, T item) {}
-    @Override public void addFirst(T item) {}
-    @Override public void addLast(T item) {}
-    @Override public T getFirst() { return null; }
-    @Override public T getLast() { return null; }
-    @Override public void removeFirst() {}
-    @Override public void removeLast() {}
+    public void set(int index, T item) {}
+    public void add(int index, T item) {}
+    public void addFirst(T item) {}
+    public void addLast(T item) {}
+    public T getFirst() { return null; }
+    public T getLast() { return null; }
+    public void removeFirst() {}
+    public void removeLast() {}
 
-    @Override
+    // Allows for-each loop
     public java.util.Iterator<T> iterator() {
         return new java.util.Iterator<T>() {
             private int current = 0;
@@ -107,6 +105,7 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
         };
     }
 
+    // Checks index is valid
     private void checkIndex(int index) {
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
     }
