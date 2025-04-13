5c1d09fa770cd5d486c5d2b449fcd75b3ac8f949
Acer <@Ramina_k>
2025-04-13 22:59:00 +0500

    Completed MyArrayList with sort, toArray, and utility methods

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,5 +1,5 @@
-// MyArrayList.java (base methods only)
-public class MyArrayList<T> implements MyList<T> {
+// MyArrayList.java (completed implementation)
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
     private Object[] data;
     private int size;
     private static final int INITIAL_CAPACITY = 10;
@@ -41,25 +41,73 @@ public class MyArrayList<T> implements MyList<T> {
         return size;
     }
 
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                T a = (T) data[j];
+                T b = (T) data[j + 1];
+                if (a.compareTo(b) > 0) {
+                    data[j] = b;
+                    data[j + 1] = a;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        System.arraycopy(data, 0, result, 0, size);
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) data[i] = null;
+        size = 0;
+    }
+
+    @Override public void set(int index, T item) {}
+    @Override public void add(int index, T item) {}
+    @Override public void addFirst(T item) {}
+    @Override public void addLast(T item) {}
+    @Override public T getFirst() { return null; }
+    @Override public T getLast() { return null; }
+    @Override public void removeFirst() {}
+    @Override public void removeLast() {}
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private int current = 0;
+            public boolean hasNext() { return current < size; }
+            public T next() { return (T) data[current++]; }
+        };
+    }
+
     private void checkIndex(int index) {
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
     }
-
-    public void set(int index, T item) {}
-    public void add(int index, T item) {}
-    public void addFirst(T item) {}
-    public void addLast(T item) {}
-    public T getFirst() { return null; }
-    public T getLast() { return null; }
-    public void removeFirst() {}
-    public void removeLast() {}
-    public void sort() {}
-    public int indexOf(Object object) { return -1; }
-    public int lastIndexOf(Object object) { return -1; }
-    public boolean exists(Object object) { return false; }
-    public Object[] toArray() { return null; }
-    public void clear() {}
-
-    public java.util.Iterator<T> iterator() { return null; }
 }
-
