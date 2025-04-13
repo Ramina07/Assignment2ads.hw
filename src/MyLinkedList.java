4d4d7cdaf3381d4bb6c2d97e2768cd1c6efb1b44
Acer <@Ramina_k>
2025-04-13 23:02:01 +0500

    Completed MyLinkedList with sort, iterator, and utility methods

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,5 +1,6 @@
-// MyLinkedList.java (base structure)
-public class MyLinkedList<T> implements MyList<T> {
+// MyLinkedList.java (finished)
+public class MyLinkedList<T extends Comparable<T>> implements MyList<T> {
+
     private class Node {
         T data;
         Node next;
@@ -13,8 +14,9 @@ public class MyLinkedList<T> implements MyList<T> {
 
     public void add(T item) {
         Node newNode = new Node(item);
-        if (tail == null) head = tail = newNode;
-        else {
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
             tail.next = newNode;
             newNode.prev = tail;
             tail = newNode;
@@ -40,24 +42,92 @@ public class MyLinkedList<T> implements MyList<T> {
     }
 
     private Node getNode(int index) {
-        Node current = head;
-        for (int i = 0; i < index; i++) current = current.next;
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Node current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) current = current.next;
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) current = current.prev;
+        }
         return current;
     }
 
-    public void set(int index, T item) {}
-    public void add(int index, T item) {}
-    public void addFirst(T item) {}
-    public void addLast(T item) {}
-    public T getFirst() { return null; }
-    public T getLast() { return null; }
-    public void removeFirst() {}
-    public void removeLast() {}
-    public void sort() {}
-    public int indexOf(Object object) { return -1; }
-    public int lastIndexOf(Object object) { return -1; }
-    public boolean exists(Object object) { return false; }
-    public Object[] toArray() { return null; }
-    public void clear() {}
-    public java.util.Iterator<T> iterator() { return null; }
+    @Override
+    public void sort() {
+        if (size < 2) return;
+        Object[] array = toArray();
+        java.util.Arrays.sort(array);
+        clear();
+        for (Object obj : array) add((T) obj);
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        Node current = head;
+        for (int i = 0; i < size; i++) {
+            result[i] = current.data;
+            current = current.next;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        Node current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.data.equals(object)) return i;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        Node current = tail;
+        for (int i = size - 1; i >= 0; i--) {
+            if (current.data.equals(object)) return i;
+            current = current.prev;
+        }
+        return -1;
+    }
+
+    @Override
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private Node current = head;
+
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+
+    @Override public void set(int index, T item) {}
+    @Override public void add(int index, T item) {}
+    @Override public void addFirst(T item) {}
+    @Override public void addLast(T item) {}
+    @Override public T getFirst() { return null; }
+    @Override public T getLast() { return null; }
+    @Override public void removeFirst() {}
+    @Override public void removeLast() {}
 }
